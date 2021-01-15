1855d4d2d9eef27e79e6f8fb83b0f03d20357770
johnniesimpson <johnniesimpson@outlook.com>
2021-01-14 21:54:39 -0500

    Refactored portfolio page to include password generator project, app and github links, and new image.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -7,7 +7,7 @@ $(document).ready(function () {
         $('#project1App').click(function (e) {
             e.preventDefault();
             window.open('https://balokdecoy.github.io/AceOfPentacles/')
-        })
+        });
     };
     function weatherApp() {
         $('#weatherCode').click(function (e) { 
@@ -17,9 +17,20 @@ $(document).ready(function () {
         $('#weatherApp').click(function (e) {
             e.preventDefault();
             window.open('https://balokdecoy.github.io/weather-app/')
-        })
+        });
+    };
+    function passwordApp() {
+        $('#passwordCode').click(function (e) { 
+            e.preventDefault();
+            window.open('https://github.com/balokdecoy/password-generator');
+        });
+        $('#passwordApp').click(function (e) {
+            e.preventDefault();
+            window.open('https://balokdecoy.github.io/password-generator/')
+        });
     };
     project1();
     weatherApp();
+    passwordApp();
 });
 
